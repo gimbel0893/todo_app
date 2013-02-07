c71d9d1d4a24ff8c3603bbb9ce476d675e4e073e
Michael Gimbel <gimbel0893@gmail.com>
2013-02-06 21:10:00 -0500

    created default rails app

---
+++ todo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoApp::Application.config.secret_token = '7b338c9ae14321eaae15ca6c56c7d95df8c061b969275b28994108aa26b2b161c37ef2f04991d17e9dac064b5811a40784d79f5bcd5aa3f5a5d1138e31bcba68'
